9965fc67d86dff74b56eff237c407293287cdd26
prammod1 <pramodhs418@gmail.com>
2024-07-20 19:28:29 +0530

    complete

---
+++ python/directory_and_file.py
@@ -0,0 +1,10 @@
+import os
+print(os.getcwd()) # get the curren directory
+print(os.getcwdb())  # in bytes
+
+#list
+print(os.listdir())
+
+#os.mkdir('test') # new directory
+#os.rename('test','new')
+#os.rmdir('new')
