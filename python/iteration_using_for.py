2c43e5175c728cd6ea57fcb690254d6e41149b9a
prammod1 <pramodhs418@gmail.com>
2024-07-26 21:44:49 +0530

    complete

---
+++ python/iteration_using_for.py
@@ -0,0 +1,18 @@
+#printing all elements present in a set
+a=[1,2,3,4,5]
+for i in a:
+    print(i, end="")
+
+
+# printing element using range function
+
+for i in range(1,10):
+    print(i)
+
+#uing for loop printing the value of present in the tuole
+
+a=(1,2,3,4,5) 
+for i in a:
+    print(i)
+else:
+    print("printing is complete")
